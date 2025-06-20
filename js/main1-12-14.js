e8deaa58045bd7da6662002a9c7ebfdf453383a3
vincencio <vinncencio@yandex.ru>
2025-06-20 16:57:57 +0300

    curry

--- js/main1-12-14.js
+++ js/main1-12-14.js
@@ -249,7 +249,24 @@ let value = eval('1+1');
 // alert(eval(val));
 // alert(eval(prompt('введите пример', '')));
 
-
+/* #1.14.3. Каррирование (перенос аргументов) */
+function curry(func) {
+    return function curried(...args) {
+        if (args.length >= func.length) {
+            return func.apply(this, args);
+        } else {
+            return function(...args2) {
+                return curried.apply(this, args.concat(args2));
+            }
+        }
+    };
+}
+// Пример использования:
+function sum(a, b, c) {return a + b + c}
+let curriedSum = curry(sum);
+console.log( curriedSum(1, 2, 3) ); // 6, всё ещё можно вызывать нормально
+console.log( curriedSum(1)(2,3) ); // 6, каррирование первого аргумента
+console.log( curriedSum(1)(2)(3) ); // 6, каррирование всех аргументов
 
 
 
