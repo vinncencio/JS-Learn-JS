7e3e789af36b66b3983af090c9507b3e1294fbce
vincencio <vinncencio@yandex.ru>
2025-07-08 16:49:16 +0300

    locales3

--- js/main1-12-14.js
+++ js/main1-12-14.js
@@ -386,10 +386,14 @@ console.log( formatter2.format(date) ); // 08.07.2025, 15:43
 	alert( animals ); // АИСТ,ёж,енот,ехидна,тигр,ЯК
 В этом примере порядок сортировки не должен зависеть от регистра. Что касается буквы "ё", то мы следуем обычным правилам сортировки буквы ё, по которым «е» и «ё» считаются одной и той же буквой, за исключением случая, когда два слова отличаются только в позиции буквы «е» / «ё» – тогда слово с «е» ставится первым.*/
 let animals = ["тигр", "ёж", "енот", "ехидна", "АИСТ", "ЯК"];
-let collator = new Intl.Collator(undefined, {sensitivity: "accent"});
+let collator = new Intl.Collator();
 animals.sort((a, b) => collator.compare(a, b))
 console.log( animals ); // (6) ['АИСТ', 'ёж', 'енот', 'ехидна', 'тигр', 'ЯК']
 
+animals = ["тигр", "ёж", "енот", "ехидна", "АИСТ", "ЯК"];
+collator = new Intl.Collator('ru', {usage: 'sort'});
+animals = animals.sort(collator.compare);
+console.log( animals ); // (6) ['АИСТ', 'ёж', 'енот', 'ехидна', 'тигр', 'ЯК']
 
 
 
