4308ad60060c93b114675561bab57ae6fdefda41
vincencio <vinncencio@yandex.ru>
2025-08-12 12:13:20 +0300

    DOM-elements

---
+++ js/main2-1.js
@@ -0,0 +1,21 @@
+// #3. 2.1.3. Навигация по DOM-элементам
+// 1. Дочерние элементы в DOM
+// console.log(document.body.firstElementChild); // <div>Пользователи:</div>
+// console.log(document.body.firstElementChild.nextElementSibling); // <ul>...</ul>
+// console.log(document.body.firstElementChild.nextElementSibling.lastElementChild); // <li>Пит</li>
+
+// 2. Вопрос о соседях
+// console.log(document.body.lastChild.nextSibling); // null
+// console.log(document.body.children[0].previousSibling); // " "
+
+// 3. Выделите ячейки по диагонали
+// td.style.backgroundColor = 'red';
+let table = document.body.firstElementChild;
+for (let i = 0; i < table.rows.length; i++){
+    table.rows[i].cells[i].style.backgroundColor = 'red';
+}
+// console.log(table.rows[0].cells.length);
+// for (let i = 0; i < table.rows.length; i++) {
+//     let row = table.rows[i];
+//     row.cells[i].style.backgroundColor = 'red';
+// }
